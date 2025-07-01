0de4aa8f65039b37126a52fba71c84f5d90bfb91
Monarch <gusoaxcell@example.com>
2025-07-01 11:26:42 +0800

    A

---
+++ app/ServiceComponent/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+
+export default function ServiceComponentPage() {
+  return (
+    <Box pt={{ base: "8", md: "14", lg: "14" }}>
+      <h1>Service Component Page</h1>
+      {/* Add your service component content here */}
+    </Box>
+  );
+}
